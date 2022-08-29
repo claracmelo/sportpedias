110fd9c4107ff27a8328dddf2bbd38b36458a723
clara-lima <claracmelo@hotmail.com>
2022-08-28 19:57:48 -0700

    CRUD works

--- controllers/sportsController.js
+++ controllers/sportsController.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router()
 const Sport = require('../models/sports.js')
-const Fact = require('../models/facts.js')
 const Contact = require('../models/contact.js')
 const Comment = require('../models/comments.js')
 
@@ -15,28 +14,40 @@ router.get('/seed', (req, res) => {
 	Sport.create(
 		[
 			{
-				sport: 'SOCCER',
+				sport: 'BASKETBALL',
 				author: 'Clara Lima', 
 				about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
-				rules: "oi", 
-				facts: "Brazil is the only 5 times champion of the world cup"},],
-		(err, data) => {
-			res.redirect('/sportpedias/sports');
-		}
-	);
-});
-
-//SEED fact http://localhost:3001/sportpedias/seedf or https://sportpedias.herokuapp.com/sportpedias/seedf
-
-router.get('/seedf', (req, res) => {
-	Fact.create(
-		[
-			{
-				name: "Clara Lima",
-				fact: "Brazil won 5 times World Cup",
-				img: "https://pbs.twimg.com/media/FEXBr0rXIA48Wy3.jpg"
-			},
-		],
+				rules: [{
+					rule:"oi"
+				}], 
+				facts: 		[
+					{
+						idFact:0,
+						name: "KEV",
+						fact: "6 World Cup",
+						img: "https://pbs.twimg.com/media/FEXBr0rXIA48Wy3.jpg"
+					},					
+					{
+						idFact:1,
+						name: "KEV",
+						fact: "6 World Cup",
+						img: "https://pbs.twimg.com/media/FEXBr0rXIA48Wy3.jpg"
+					},
+				],
+					comments:
+					[
+							{
+								name: "clara",
+								anonymous: true,
+								comment: "primeiro comentario"
+							},
+							{
+								name: "kevin",
+								anonymous: true,
+								comment: "second comentario"
+							}
+					]
+			}],
 		(err, data) => {
 			res.redirect('/sportpedias/sports');
 		}
@@ -154,34 +165,39 @@ router.post('/', (req, res) => {
 //					COMMENT
 //----------------------------------------------------------
 
-//SHOW CONTACT -- WORKS
-router.get('/comment', (req, res) => {
-	res.render('comment.ejs');
+
+// // NEW FACT -- WORKS
+router.get('/sports/:id/comments/new', (req, res) => {
+	const sport = Sport.findById(req.params.id);
+	res.render('newComment.ejs', { 	
+		sport: sport,
+	});
 });
 
-//CREATE COMMENT
-	router.post('/', (req, res) => {
-		if (req.body.anonymous === 'on') {
-			req.body.anonymous = true;
-		} else {
-			req.body.anonymous = false;
-		}
-	
-		Comment.create(req.body, (error, createComment) => {
-			if (error) {
-				console.log('error', error);
-				res.send(error);
-			} else {
-				res.redirect('/sportpedias');
-			}
-		});
-	});
-	
+//CREATE COMMENT - WORKS
+router.put('/sports/:id/comments', (req, res) => {
 
+	if (req.body.anonymous === 'on') {
+		req.body.anonymous = true;
+		req.body.name = "Anonymous"
+	} else {
+		req.body.anonymous = false;
+	}
+	let newComment = {name:req.body.name,anonymous:req.body.anonymous,comment:req.body.comment};
+	console.log("comment",newComment);
+	Sport.findByIdAndUpdate({_id:req.params.id},{$push:{"comments":[newComment]}},
+		function(err, result) {
+
+			console.log("array",result)
+	res.redirect(`/sportpedias/sports/${req.params.id}`)
+	});
+});
+
+	
 //----------------------------------------------------------
 //						RULES
 //----------------------------------------------------------
-// //SHOW RULES
+// //INDEX RULES
 router.get('/sports/:id/rules', async (req, res) => {
 	const sport = await Sport.findById(req.params.id);
 	res.render('rules.ejs', {
@@ -189,58 +205,107 @@ router.get('/sports/:id/rules', async (req, res) => {
 	});
 });
 
+// NEW RULES
+router.get('/sports/:id/rules/new', (req, res) => {
+	const sport = Sport.findById(req.params.id);
+	res.render('newRules.ejs', { 	
+		sport: sport,
+	});
+});
+
+// CREATE RULES 
+router.put('/sports/:id/rules', (req, res) => {
+	let newRule= {rule:req.body.rule};
+	Sport.findByIdAndUpdate({_id:req.params.id},{$push:{rules:[newRule]}},
+		function(err, result) {
+			console.log("array",result)
+	res.redirect(`/sportpedias/sports/${req.params.id}/rules`)
+	});
+});
+//SHOW RULES - WORKS
+router.get('/sports/:id/rules/:idr', async (req, res) => {
+	const sport = await Sport.findById(req.params.idr);
+	res.render('rules.ejs', {
+		sport: sport
+	});
+});
+
+// // UPDATE FACT -- WORKS
+router.put('/sports/:id/rules/:idr', (req, res) => {
+	Sport.findByIdAndUpdate({_id:req.params.id},{$set:{["rules.$[el]"]:req.body}},{
+		  arrayFilters: [{ "el._id": req.params.idr}],
+		  new: true}, (err, updatedModel) => {
+	res.redirect(`/sportpedias/sports/${req.params.id}/rules`)
+	})
+})
 //----------------------------------------------------------
 //						FACT
 //----------------------------------------------------------
 
 // //INDEX FACT - WORKS
 router.get('/sports/:id/facts', async (req, res) => {
-	const sports = await Sport.findById(req.params.id);
-	// const fact = await Fact.findById(req.params.idf);
-	const facts = await Fact.find({});
+	const sports = Sport.find({});
+	const sport = await Sport.findById(req.params.id);
 	res.render('fact.ejs', { 	
-		// fact: fact,	
-		facts: facts,
-		sports: sports
+		sport: sport,sports:sports
+	});
+});
+
+// // NEW FACT -- WORKS
+router.get('/sports/:id/facts/new', (req, res) => {
+	const sport = Sport.findById(req.params.id);
+	res.render('newFact.ejs', { 	
+		sport: sport,
+	});
+});
+
+// CREATE FACT - WORKS
+router.put('/sports/:id/facts', (req, res) => {
+	let newFact = {name:req.body.name,fact:req.body.fact,img:req.body.img};
+	console.log("fact",newFact);
+	Sport.findByIdAndUpdate({_id:req.params.id},{$push:{"facts":[newFact]}},
+		function(err, result) {
+			console.log("array",result)
+	res.redirect(`/sportpedias/sports/${req.params.id}/facts`)
 	});
 });
 
 //SHOW FACT - WORKS
 router.get('/sports/:id/facts/:idf', async (req, res) => {
-	const fact = await Fact.findById(req.params.idf);
-	const sport = await Sport.findById(req.params.id);
+	const sport = await Sport.findById(req.params.idf);
 	res.render('fact.ejs', {
-		fact: fact,
 		sport: sport
 	});
 });
 
 // DELETE FACT -- WORKS
 router.delete('/sports/:id/facts/:idf', (req, res) => {
-	Fact.findByIdAndRemove(req.params.idf, (err, data)=> {
-		if(err) 
-		console.log("error to delete",err)
+		Sport.findByIdAndUpdate({_id:req.params.id},{$pull:{facts:{_id:req.params.idf}}},{
+			  new: true}, (err, updatedModel) => {
 		res.redirect(`/sportpedias/sports/${req.params.id}/facts`)
+		})
 	})
-})
 
 // // EDIT FACT -- WORKS
 router.get('/sports/:id/facts/:idf/edit', (req, res) => {
-	const sport = Sport.findById(req.params.id);
-	Fact.findById(req.params.idf, (err, factFound) => {
-		res.render('editF.ejs', {fact: factFound, sport:sport})
+	Sport.findById(req.params.id, (err,factFound) => {
+		res.render('editF.ejs', {fact:factFound,index:req.params.idf})
 	})
 })
 
 // // UPDATE FACT -- WORKS
 router.put('/sports/:id/facts/:idf', (req, res) => {
-	const sport = Sport.findById(req.params.id);
-	Fact.findByIdAndUpdate(req.params.idf, req.body, {new:true}, (err, updatedModel) => {
-		
-		sport:sport
-		res.redirect(`/sportpedias/sports/${req.params.id}/facts`)
+	Sport.findByIdAndUpdate({_id:req.params.id},{$set:{["facts.$[el]"]:req.body}},{
+		  arrayFilters: [{ "el._id": req.params.idf}],
+		  new: true}, (err, updatedModel) => {
+	res.redirect(`/sportpedias/sports/${req.params.id}/facts`)
 	})
 })
 
-
 module.exports = router
+
+
+
+
+
+
--- models/sports.js
+++ models/sports.js
@@ -2,12 +2,26 @@ const mongoose = require("mongoose")
 
 //CRETE SCHEMA SPORTS
 const sportSchema = new mongoose.Schema({
-    sport: {type:String, required:true},
-    author: {type:String, required:true}, 
+    sport: {type:String},
+    author: {type:String}, 
     about: String,
-    rules: String, 
-    facts: String, 
-});
+    rules: [{rule: {type:String}}], 
+    facts: [{ 
+            name: {type:String},
+            fact: {type:String},
+            img: {type:String}
+           }],
+
+        //    facts: [{
+        //         type: mongoose.Schema.Types.ObjectId,
+        //         ref: 'Fact',
+        // }],
+    comments:[{
+            name: String,
+            anonymous: Boolean, 
+            comment: String
+           }]
+        });
 
 //CREATE MODEL SPORTS
 const Sport= mongoose.model("sport",sportSchema);
